00f80b269cd08ded2628ff7936498da62664eb48
arielbilaperez-crypto <arielbilaperez@gmail.com>
2026-06-05 21:33:57 +0200

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -54,7 +54,7 @@ export default async function handler(req, res) {
     }
     if (vehicle === "bmw_g_lci") {
   baseImage =
-    "https://volantcustom.be/cdn/shop/files/FullSizeRender_adf856b6-a07c-42f5-a017-fe24d2a977c5.jpg?v=1780687506&width=990";
+    "https://volantcustom.be/cdn/shop/files/bmwgseries_vcbcover.jpg?v=1766945763&width=990";
   vehiclePrompt =
     "BMW G-Series Facelift 2025 M Sport steering wheel (G20 LCI, G80 LCI), modern OEM design";
     }
