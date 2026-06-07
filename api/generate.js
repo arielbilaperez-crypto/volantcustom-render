f8ce947a2b4f76f81f2555d2dc8e58b7aef68669
arielbilaperez-crypto <arielbilaperez@gmail.com>
2026-06-07 02:49:17 +0200

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
 
     if (vehicle === "golf8") {
       baseImage =
-        "https://volantcustom.be/cdn/shop/files/IMG-5968.png?v=1767471751&width=990";
+        "https://res.cloudinary.com/dssr4em6b/image/upload/v1780792645/IMG-5968_rhz4ma.webp";
       vehiclePrompt =
         "Volkswagen Golf 8 GTI / R steering wheel, modern sporty design";
     }
@@ -48,7 +48,7 @@ export default async function handler(req, res) {
     }
     if (vehicle === "bmw_g") {
   baseImage =
-    "https://volantcustom.be/cdn/shop/files/bmwgseries_vcbcover.jpg?v=1766945763&width=990";
+    "https://res.cloudinary.com/dssr4em6b/image/upload/v1780792700/bmwgseries_vcbcover_mvmefo.webp";
   vehiclePrompt =
     "BMW G-Series M Sport steering wheel (G20, G30, G42), modern OEM design";
     }
@@ -60,49 +60,49 @@ export default async function handler(req, res) {
     }
     if (vehicle === "audi_8v1") {
   baseImage =
-    "https://volantcustom.be/cdn/shop/files/audi_oldgen_vcbcover.webp?v=1766944712&width=990";
+    "https://res.cloudinary.com/dssr4em6b/image/upload/v1780792874/audi_oldgen_vcbcover_1_lhg3pc.webp";
   vehiclePrompt =
     "Audi 8V1 (A3/S3/RS3) steering wheel, modern OEM design";
     }
     if (vehicle === "audi_8v2") {
   baseImage =
-    "https://volantcustom.be/cdn/shop/files/96169F12-4407-4613-83CD-14A1E6C59266.png?v=1767142188&width=990";
+    "https://res.cloudinary.com/dssr4em6b/image/upload/v1780792757/96169F12-4407-4613-83CD-14A1E6C59266_pqqbpq.webp";
   vehiclePrompt =
     "Audi 8V2 (A3/A4/A5/S3/S5/RS3) steering wheel, modern OEM design";
     }
     if (vehicle === "audi_8y") {
   baseImage =
-    "https://volantcustom.be/cdn/shop/files/audiB9.5_vcbcover.jpg?v=1766943905&width=990";
+    "https://res.cloudinary.com/dssr4em6b/image/upload/v1780793047/audiB9.5_vcbcover_xay9tt.webp";
   vehiclePrompt =
     "Audi 8Y B9.5 (A3/A4/A5/RS3 8Y) steering wheel, modern OEM design";
     }
     if (vehicle === "audi_rs6") {
   baseImage =
-    "https://volantcustom.be/cdn/shop/files/audirs6c8_vcbcover.webp?v=1766947523&width=990";
+    "https://res.cloudinary.com/dssr4em6b/image/upload/v1780793302/audirs6c8_vcbcover_1_f0vsuy.webp";
   vehiclePrompt =
     "Audi (RS6/A7/RS7 C8) steering wheel, modern OEM design";
     }
     if (vehicle === "audi_r8") {
   baseImage =
-    "https://volantcustom.be/cdn/shop/files/audirs38v2_vcbcover.jpg?v=1766944362&width=990";
+    "https://res.cloudinary.com/dssr4em6b/image/upload/v1780793189/audirs38v2_vcbcover_uprzmj.webp";
   vehiclePrompt =
     "Audi 8V2 (TT/TTRS/R8) steering wheel, modern OEM design";
     }
     if (vehicle === "mb_sport") {
   baseImage =
-    "https://volantcustom.be/cdn/shop/files/mercedes2015_vcbcover.webp?v=1766943723&width=990";
+    "https://res.cloudinary.com/dssr4em6b/image/upload/v1780792958/mercedes2015_vcbcover_1_samqnz.webp";
   vehiclePrompt =
     "Mercedes AMG Performance 2010-2014 (C63 W204/ E63 W212) steering wheel, modern OEM design";
     }
     if (vehicle === "mb_super") {
   baseImage =
-    "https://volantcustom.be/cdn/shop/files/mercedesamg2018_vcbcover.webp?v=1766943148&width=990";
+    "https://res.cloudinary.com/dssr4em6b/image/upload/v1780792813/mercedesamg2018_vcbcover_1_gjjusi.webp";
   vehiclePrompt =
     "Mercedes AMG Sport 2014-2019 (C/E/SL/CL/CLS/GLC/AMG GT) steering wheel, modern OEM design";
     }
     if (vehicle === "mb_ultra") {
   baseImage =
-    "https://volantcustom.be/cdn/shop/files/vcb_mercedesamg2023_fullwhite.webp?v=1766943521&width=990";
+    "https://res.cloudinary.com/dssr4em6b/image/upload/v1780793125/vcb_mercedesamg2023_fullwhite_1_tx35rw.webp";
   vehiclePrompt =
     "Mercedes AMG Sport 2020-2024 (W206, W213) steering wheel, modern OEM design";
     }
